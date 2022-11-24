9b030a52569f451d109fa62f0ce3725fc8865f79
ricardinho <ricardolopes18@yahoo.com.br>
2022-11-24 14:56:13 +0100

    Removendo prints the debug

--- app/main.py
+++ app/main.py
@@ -37,7 +37,6 @@ def index():
 
 @app.get('/api/v1/gastos/{mes}/{ano}/', response_model=schemas.Gastos)
 def read_gastos(mes: int, ano: int, db: Session = Depends(get_db)):
-
     if mes is None or ano is None:
         raise HTTPException(status_code=404, detail="Mes or Ano not found")
 
@@ -47,20 +46,17 @@ def read_gastos(mes: int, ano: int, db: Session = Depends(get_db)):
 
 @app.post('/api/v1/api/gastos', response_model=schemas.Gastos)
 def criar_gasto(gasto: schemas.Gastos, db: Session = Depends(get_db)):
-
     gastos = api.create_gasto(db=db, gasto=gasto)
     return gastos
 
 
 @app.get("/api/v1/fixos/{mes}/{ano}/", response_model=schemas.Fixos)
 def read_fixos(mes: int, ano: int, db: Session = Depends(get_db)):
-
     fixos = api.get_fixos(db, mes=mes, ano=ano)
     return fixos
 
 
 @app.get("/api/v1/variaveis/{mes}/{ano}/", response_model=schemas.Variaveis)
 def read_variaveis(mes: int, ano: int, db: Session = Depends(get_db)):
-
     variaveis = api.get_variaveis(db, mes=mes, ano=ano)
     return variaveis
